87b7f0d4bd73e54848dc386d1a156569687682d2
Diego Maceda <dmaceda2288@gmail.com>
2022-11-20 10:53:53 -0300

    transition added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Access from "./screens/Access";
 import Home from "./screens/Home";
 import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
     <div>
       <AnimatePresence mode="wait" initial={false}>
-        <Routes>
+        <Routes location={location}>
           <Route path="/" element={<Home />} />
           <Route path="/access" element={<Access />} />
         </Routes>
